533a9b470585b5630ad941bc325e3cd0383028c2
Kos8943 <a26095998@gmail.com>
2020-09-03 10:40:09 +0800

    product-list navbar

---
+++ proj/product-list.php
@@ -0,0 +1,5 @@
+<?php require __DIR__ . '/../part/__connect_db.php' ?>
+<?php include __DIR__ . '/../part/__navbar.php' ?>
+<?php include __DIR__ . '/../part/__html_head.php' ?>
+<?php include __DIR__ . '/../part/__scripts.php' ?>
+<?php require __DIR__ . '/../part/__html_foot.php' ?>
